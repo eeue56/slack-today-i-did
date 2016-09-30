25033b21735ab9ba551fea5fd530a5e93d38c9b4
eeue56 <enalicho@gmail.com>
2016-09-30 02:36:24 +0200

    add rollbar api

---
+++ rollbar.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class Rollbar(object):
+    def __init__(self, token):
+        self.token = token
+        self.base_url = "https://api.rollbar.com"
+
+    def request(self, url):
+        actual_url = self.base_url + url + f"?access_token={self.token}"
+        print(actual_url)
+        return requests.get(actual_url)
+
+    def get_item_by_id(self, id):
+        response = self.request(f'/api/1/item/{id}')
+
+        json = response.json()
+
+        return json
+
+    def get_item_by_counter(self, counter):
+        response = self.request(f'/api/1/item_by_counter/{counter}')
+
+        json = response.json()
+
+        return json['result']
