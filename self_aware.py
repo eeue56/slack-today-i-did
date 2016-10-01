4ffae07eb8da6159eae67c90aba2605acbb1d542
Noah <eeue56@users.noreply.github.com>
2016-10-01 04:13:04 +0100

    add a self aware module

---
+++ self_aware.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import psutil
+import logging
+
+def restart_program():
+    """Restarts the current program, with file objects and descriptors
+       cleanup
+    """
+
+    try:
+        p = psutil.Process(os.getpid())
+        for handler in p.get_open_files() + p.connections():
+            os.close(handler.fd)
+    except Exception as e:
+        logging.error(e)
+
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
