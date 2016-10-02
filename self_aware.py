bc68e5adf03ce61dd6b6b46907441df170f1cbb4
eeue56 <enalicho@gmail.com>
2016-10-02 03:35:16 +0200

    checkout branch

--- self_aware.py
+++ self_aware.py
@@ -5,7 +5,7 @@ import logging
 
 def git_checkout(branch):
     os.system('git pull')
-    os.system(f'git checkout origin/{branch}')
+    os.system(f'git checkout {branch}')
 
 def restart_program():
     """Restarts the current program, with file objects and descriptors
