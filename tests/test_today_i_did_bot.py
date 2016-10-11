4c67e0b21971dbd73206ba310f58c0a175a9524f
noah <noah@noredink.com>
2016-10-11 04:18:36 +0100

    set last sender

--- tests/test_today_i_did_bot.py
+++ tests/test_today_i_did_bot.py
@@ -12,9 +12,10 @@ def test_session(mocker):
     mocked_channel_message = mocker.patch.object(TodayIDidBot, 'send_channel_message')
     mocker.patch.object(TodayIDidBot, 'was_directed_at_me', return_value=True)
     mocker.patch.object(TodayIDidBot, 'user_name_from_id', return_value=MOCK_PERSON)
-    mocker.patch.object(TodayIDidBot, 'connected_user', return_value=MOCK_PERSON)
+    mocker.patch.object(TodayIDidBot, 'connected_user', return_value='TodayIDidBot')
 
     bot = TodayIDidBot('', rollbar_token='', elm_repo=None)
+    bot._last_sender = MOCK_PERSON
 
     bot.parse_direct_message({
         'user': MOCK_PERSON,
@@ -22,5 +23,6 @@ def test_session(mocker):
         'text': MOCK_START_SESSION_TEXT
         })
 
-    assert 'Started a session for you' in mocked_channel_message.call_args[0]
+    assert MOCK_CHANNEL == mocked_channel_message.call_args[0]
+    assert 'Started a session for you' in mocked_channel_message.call_args[1]
     assert mocked_channel_message.call_count == 1
