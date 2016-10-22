b7ec982db71614eee32ca1ed374a73d9955b5505
Noah <eeue56@users.noreply.github.com>
2016-10-22 04:27:03 +0100

    add more tests for help suggestions (#22)

--- tests/test_generic_bot.py
+++ tests/test_generic_bot.py
@@ -39,3 +39,33 @@ def test_help_on_func(mocker, bot, message_context):
     assert MOCK_CHANNEL == mocked_channel_message.call_args[0][0]
     assert "I'll tell you about it" in mocked_channel_message.call_args[0][1]
     assert mocked_channel_message.call_count == 1
+
+
+def test_help_on_mispelled_func(mocker, bot, message_context):
+    mocked_channel_message = mocker.patch.object(bot, 'send_channel_message')
+
+    with message_context(bot, sender=MOCK_PERSON):
+        bot.parse_direct_message({
+            'user': MOCK_PERSON,
+            'channel': MOCK_CHANNEL,
+            'text': 'help helc'
+        })
+
+    assert MOCK_CHANNEL == mocked_channel_message.call_args[0][0]
+    assert "I did find the following functions" in mocked_channel_message.call_args[0][1]
+    assert mocked_channel_message.call_count == 1
+
+
+def test_help_on_mispelled_something(mocker, bot, message_context):
+    mocked_channel_message = mocker.patch.object(bot, 'send_channel_message')
+
+    with message_context(bot, sender=MOCK_PERSON):
+        bot.parse_direct_message({
+            'user': MOCK_PERSON,
+            'channel': MOCK_CHANNEL,
+            'text': 'help hdebf8u3ijr9jndsiix'
+        })
+
+    assert MOCK_CHANNEL == mocked_channel_message.call_args[0][0]
+    assert "I don't know what you mean and have no suggestions" in mocked_channel_message.call_args[0][1]
+    assert mocked_channel_message.call_count == 1
