e938fbd278b5335b66c38a1f67e3f7fb157e5811
noah <noah@noredink.com>
2016-11-03 21:45:35 +0000

    echo out info on how far the script is through

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env bash
 set -e
 
+echo "importing main..."
 # make sure that there's no syntax errors
 python3 -c "import main"
 
+echo "running flake8..."
 # run flake8 on everything
 flake8 main.py slack_today_i_did
 
+echo "running doctest..."
 # run doctest
 python3 -m doctest slack_today_i_did/*.py
 
+echo "running pytest..."
 python3 -m pytest
